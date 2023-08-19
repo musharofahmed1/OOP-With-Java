e2a9e4bb1d241af57a889b8fba987a1e846411b6
musharofahmed1 <musharofahmed11@gmail.com>
2023-08-19 12:09:12 -0600

    whileCounter function added

--- whileCounter.java
+++ whileCounter.java
@@ -1,3 +1,11 @@
 public class whileCounter {
-    
+    public static void main(String[] args){
+        int counter = 1;
+
+        while(counter <= 10){
+            System.out.printf("%d  ",counter);
+            ++counter;
+        }
+        System.out.println();
+    }
 }
